1ef5e104863cc68e1dbf3537b1d80ad1da967f63
Inna Botvinnik <21icelady97@gmail.com>
2018-01-30 20:40:23 +1000

    final version for checking (+ adaptive)

---
+++ js/form.js
@@ -0,0 +1,6 @@
+function ml_webform_success_7558960() {
+                var $ = ml_jQuery || jQuery;
+
+                $('.ml-subscribe-form-7558960 .ml-block-success').show();
+                $('.ml-subscribe-form-7558960 .ml-block-form').hide();
+            };
